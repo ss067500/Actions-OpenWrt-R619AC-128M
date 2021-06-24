56125bfd32c5ee1af655e72fcad01ddda2e19d82
ss067500 <59464268+ss067500@users.noreply.github.com>
2021-06-24 16:05:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,6 +14,8 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 echo 'JD添加软件源'
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package
+
+git clone https://github.com/fw876/helloworld package
 #echo '添加软件源'
 #git clone https://github.com/liuran001/openwrt-packages package
 
