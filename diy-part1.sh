6f6aa1b6d63b43869e73c8959afb295b3c623129
ss067500 <59464268+ss067500@users.noreply.github.com>
2022-04-26 15:09:33 +0800

    update测试
    update测试

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,8 +10,37 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
+#获取Lienol-xiaorouji-passwall
+# git clone https://github.com/xiaorouji/openwrt-package/lienol/ package/diy-packages/lienol
+# git clone https://github.com/xiaorouji/openwrt-package/tree/master/lienol/luci-app-passwall package/luci-app-passwall
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+git clone https://github.com/kenzok8/small.git package/small
+git clone https://github.com/open-mesh-mirror/batman-adv.git package/batman-adv
+
+# 增加ssr（上述kenzok8/openwrt-packages已包含）
+# git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+# git clone https://github.com/fw876/helloworld.git package/diy-packages/luci-app-ssr-plus
+
+#  增加luci-app-unblockneteasemusic，来源immortalwrt
+git clone https://github.com/immortalwrt/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
+
+
+# CONFIG_PACKAGE_luci-app-filebrowser=y
+#git clone https://github.com/immortalwrt/openwrt-filebrowser.git package/luci-app-filebrowser
+
+
 #Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+#DDNSTO
+echo >> feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+./scripts/feeds update nas nas_luci
+./scripts/feeds install -a -p nas
+./scripts/feeds install -a -p nas_luci 
+#
+#
