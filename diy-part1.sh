84f93608cb989d6da3fddabbc011668ba363b111
ss067500 <sun067500@163.com>
2022-12-15 15:17:56 +0800

    更新 diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,8 +9,11 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-
-
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
+git clone https://github.com/kenzok8/small.git package/small
+git clone https://github.com/open-mesh-mirror/batman-adv.git package/batman-adv
 
 # Add a feed source
 sed -i "/helloworld/d" "feeds.conf.default"
