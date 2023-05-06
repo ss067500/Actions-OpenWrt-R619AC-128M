bffe7f38aafe3a5903c1ff060d70cb1a9cf78e05
ss067500 <59464268+ss067500@users.noreply.github.com>
2023-05-07 00:26:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,7 +21,7 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld.git'>> feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
 echo 'src-git kenzok https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 #sed -i '$a src-git Aibx https://github.com/Aibx/openwrt-packages' feeds.conf.default
 
 #Add DDNSTO
