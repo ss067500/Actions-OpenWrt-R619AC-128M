d9ad7abc73015862b9014148a0bca78824cc2016
ss067500 <59464268+ss067500@users.noreply.github.com>
2023-09-02 07:23:43 -0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,7 +20,7 @@ echo '修改机器名称'
 sed -i 's/OpenWrt/H3399PC/g' package/base-files/files/bin/config_generate
 
 echo '修改网关地址'
-sed -i 's/192.168.10.1/192.168.22.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.22.1/g' package/base-files/files/bin/config_generate
 
 echo '修改默认主题'
 sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
