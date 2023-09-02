405a3736e629600ea989eccafa23d73f7dcc8bf3
ss067500 <59464268+ss067500@users.noreply.github.com>
2023-09-02 07:22:54 -0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,21 +9,18 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-#echo '下载helloworld-补丁'
-#wgit https://codeload.github.com/zfl9/ipt2socks/tar.gz/v1.1.3?/ipt2socks-1.1.3.tar.gz openwrt/dl
+
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-#echo '下载helloworld'
-#git clone https://github.com/fw876/helloworld package/lean/helloworld
 
 #echo '添加软件源'
 #git clone https://github.com/liuran001/openwrt-packages package
 
 echo '修改机器名称'
-sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/H3399PC/g' package/base-files/files/bin/config_generate
 
 echo '修改网关地址'
-sed -i 's/192.168.1.1/192.168.22.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.10.1/192.168.22.1/g' package/base-files/files/bin/config_generate
 
 echo '修改默认主题'
 sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
@@ -32,7 +29,13 @@ echo '去除默认bootstrap主题'
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 echo '修改wifi名称'
-sed -i 's/OpenWrt/G-DOCK/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/OpenWrt/H3399/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+
+
+
+
+
 
 
 
