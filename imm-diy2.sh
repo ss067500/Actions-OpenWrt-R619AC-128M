77927b387f4d9567f20b06f9be06fc4440049f96
ss067500 <sun067500@163.com>
2023-10-19 14:40:51 +0800

    更新 imm-diy2.sh

--- imm-diy2.sh
+++ imm-diy2.sh
@@ -23,7 +23,7 @@ echo '修改机器名称'
 sed -i 's/ImmortalWrt/G-DOCK/g' package/base-files/files/bin/config_generate
 
 echo '修改网关地址'
-sed -i 's/192.168.1.1/192.168.22.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 echo '修改默认主题'
 sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
