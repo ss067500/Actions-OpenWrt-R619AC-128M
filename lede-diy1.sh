7441b6396881249e285c60fed31f536e26d62249
ss067500 <59464268+ss067500@users.noreply.github.com>
2024-03-20 15:09:42 +0800

    Update lede-diy1.sh

--- lede-diy1.sh
+++ lede-diy1.sh
@@ -18,7 +18,7 @@
 
 # Add a feed source
 #sed -i "/helloworld/d" "feeds.conf.default"
-#echo 'src-git helloworld https://github.com/fw876/helloworld.git'>> feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld.git'>> feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
 echo 'src-git kenzok https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
